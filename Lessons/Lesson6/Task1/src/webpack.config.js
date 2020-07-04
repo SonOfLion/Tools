ee470e299baa33f4018a4006c9fd2135f9c93642
SonOfLion <syplusplus@gmail.com>
2020-07-05 00:48:28 +0300

    1

--- Lessons/Lesson6/Task1/src/webpack.config.js
+++ Lessons/Lesson6/Task1/src/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     },
     module: {
         rules: [{
-                test: /.css$/,
+                test: /.s?css$/,
                 use: ['style-loader', 'css-loader']
             },
             {
