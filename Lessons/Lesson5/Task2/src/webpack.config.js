7ac1e52d691de026674c4dcb30834f1d28ce1c59
SonOfLion <syplusplus@gmail.com>
2020-07-04 20:24:16 +0300

    2

---
+++ Lessons/Lesson5/Task2/src/webpack.config.js
@@ -0,0 +1,13 @@
+const path = requare('path');
+
+module.export = {
+    entry: {
+        profile: './src/profile/index.js',
+        dashboard: './src/dashboard/index.js',
+    },
+    output: {
+        filename: '[name].js',
+        path: path.join(__dirname, './build')
+    },
+    watch: true
+};
