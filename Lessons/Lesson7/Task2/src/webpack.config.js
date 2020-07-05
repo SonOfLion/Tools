51aea347802671e59cc943b8fa7b8972e06bcdab
SonOfLion <syplusplus@gmail.com>
2020-07-05 14:45:27 +0300

    2

--- Lessons/Lesson7/Task2/src/webpack.config.js
+++ Lessons/Lesson7/Task2/src/webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
         ],
     },
     plugins: [
-        new CleanWebpackPlugin();
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: './src/index.html'
         }),
