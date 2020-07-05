789aae06dd367db43f0ac0984ef7c41cfc3441ed
SonOfLion <syplusplus@gmail.com>
2020-07-05 17:22:32 +0300

    1

--- Lessons/Lesson8/Task1/src/webpack.config.js
+++ Lessons/Lesson8/Task1/src/webpack.config.js
@@ -1,40 +1,49 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-                test: /\.s?css$/i,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+module.exports = (env, argv) => {
+        const isProduction = argv.mode === 'production';
+        const config = {
+            entry: './src/index.js',
+            output: {
+                filename: 'bundle.js'
             },
-            {
-                test: /\.(png|jpg|gif)$/i,
-                use: [{
-                    loader: 'url-loader',
-                    options: {
-                        limit: 8192,
-                        name: '[name].[ext]',
-                        outputPath: 'images',
+            module: {
+                rules: [{
+                        test: /\.s?css$/i,
+                        use: [
+                            isProduction ? MiniCssExtractPlugin.loader :
+                            'style-loader', 'css-loader', 'sass-loader'
+                        ]
                     },
-                }, ],
-            }
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-        })
-    ],
-    devServer: {
-        port: 9000,
-        hot: true,
+                    {
+                        test: /\.(png|jpg|gif)$/i,
+                        use: [{
+                            loader: 'url-loader',
+                            options: {
+                                limit: 8192,
+                                name: '[name].[ext]',
+                                outputPath: 'images',
+                            },
+                        }, ],
+                    }
+                ],
+            },
+            plugins: [
+                new HtmlWebpackPlugin({
+                    template: './src/index.html'
+                }),
+            ],
+            devServer: {
+                port: 9000,
+                hot: true,
 
-    }
-};
+            }
+
+        };
+        if (isProduction) {
+            config.plugins.push(new MiniCssExtractPlugin({
+                filename: '[name].css',
+            }));
+            return config;
+        };
