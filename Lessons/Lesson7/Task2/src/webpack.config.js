748642f194500eb37967230e43e6c59c7a2e29c0
SonOfLion <syplusplus@gmail.com>
2020-07-05 14:47:34 +0300

    2

--- Lessons/Lesson7/Task2/src/webpack.config.js
+++ Lessons/Lesson7/Task2/src/webpack.config.js
@@ -1,6 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { ProgressPlugin } = require('webpack');
+const webpack = require('webpack');
 
 
 module.exports = {
@@ -31,6 +33,7 @@ module.exports = {
         ],
     },
     plugins: [
+        new webpack, ProgressPlugin(),
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: './src/index.html'
