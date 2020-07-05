8d0d2f5ea7bf1dae0495c5dc784853d6db5e8663
SonOfLion <syplusplus@gmail.com>
2020-07-05 19:26:42 +0300

    1

---
+++ Lessons/Lesson9/Task1/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: 'eslint-config-airbnb-base',
+    // plugins:[]
+    rules: {
+        'no-console': 1,
+        eqeqeq: 2
+    },
+    parserOptions: {
+        ecmaVersion: 10,
+        sourceType: 'module'
+    }
+};
